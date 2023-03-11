42ba241b00313bcb4ebe5b9bc24780f35890bddc
raied Abdelgani <raiedabdelgani@gmail.com>
2023-03-11 13:59:47 -0500

    routes, css, footer

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from "react";
 import Home from "./components/home/home";
 import Resume from "./components/resume/resume";
 import Contact from "./components/contact/contact";
-import footer from "./components/footer";
+import Footer from "./components/footer";
 import About from "./components/about/about";
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <About />
-      <Resume />
-      <Contact />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
